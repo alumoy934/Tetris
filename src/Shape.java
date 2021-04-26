dbf25cc1ad9055616bb2db0d3105b2bc9caff331
carlosmoyano <cmoyanomoreno@gmail.com>
2021-04-26 13:29:37 +0200

    Tetris

--- src/Shape.java
+++ src/Shape.java
@@ -29,5 +29,95 @@ public class Shape {
         pieceShape = shapeType;
     }
 
+    private void setX (int index, int x){
+        coords[index][0] = x;
+    }
+
+    private void setY (int index, int y){
+        coords[index][1] = y;
+    }
+
+    public int getX (int index){
+        return coords[index][0];
+    }
+
+    public int getY (int index){
+        return coords[index][1];
+    }
+
+    public Tetrominoes getShape(){
+        return pieceShape;
+    }
+
+    public void setRandonShape(){
+        int number = (int) (Math.random()*7+1);
+        Tetrominoes[] values =	Tetrominoes.values();
+        setShape(values[number]);
+    }
+
+    public int minX()
+    {
+        int m = coords[0][0];
+        for (int i=0; i < 4; i++) {
+            m = Math.min(m, coords[i][0]);
+        }
+        return m;
+    }
+
+    public int minY(){
+        int m = coords[0][1];
+        for (int i = 0; i < 4; i++) {
+            m = Math.min(m, coords[i][1]);
+        }
+        return m;
+    }
+
+    public int maxX(){
+        int m = coords[0][0];
+        for (int i = 0; i < 4; i++) {
+            m = Math.max(m,coords[i][0]);
+        }
+        return m;
+    }
+    public int maxY(){
+        int m = coords[0][1];
+        for (int i = 0; i < 4; i++) {
+            m = Math.max(m, coords[i][1]);
+        }
+        return m;
+    }
+
+    public Shape rotateLeft(){
+        if (pieceShape == Tetrominoes.SquareShape){
+            return this;
+        }
+
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; i++) {
+            result.setX(i, -coords[i][1]);
+            result.setY(i, coords[i][0]);
+        }
+
+        return result;
+    }
+
+    public Shape rotateRight(){
+        if (pieceShape == Tetrominoes.SquareShape){
+            return this;
+        }
+
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; i++) {
+            result.setX(i, coords[i][1]);
+            result.setY(i, -coords[i][0]);
+        }
+
+        return result;
+    }
+
 
 }
